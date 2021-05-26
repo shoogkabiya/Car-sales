bc0dd29cbee1c9d98d76f3ce093b1301460ddb0d
shoogkabiya <shoogkabiya@hotmail.com>
2021-05-26 17:14:28 +0300

    Form input validation

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -2,13 +2,15 @@ import { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 
 const Signin = () => {
+  const [errorsusername, setErrorsUsername] = useState("");
+  const [errorspassword, setErrorsPassword] = useState("");
   const history = useHistory();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isValidUsername, setIsValidUsername] = useState(false);
   const [isValidPassword, setIsValidPassword] = useState(false);
-  const [messageUsername, setMessageUsername] = useState("");
-  const [messagePassword, setMessagePassword] = useState("");
+  // const [messageUsername, setMessageUsername] = useState("");
+  // const [messagePassword, setMessagePassword] = useState("");
   const validateUsername = (event) => {
     const patternUsername = /^[a-z\d]{5,12}$/i;
     setUsername(event.target.value);
@@ -16,7 +18,7 @@ const Signin = () => {
       setIsValidUsername(true);
     } else {
       setIsValidUsername(false);
-      setMessageUsername("Please enter a  valid username");
+      setErrorsUsername("Invalid  username");
     }
   };
 
@@ -27,7 +29,7 @@ const Signin = () => {
       setIsValidPassword(true);
     } else {
       setIsValidPassword(false);
-      setMessagePassword("Please enter a  valid password");
+      setErrorsPassword("Invalid  password");
     }
   };
 
@@ -62,10 +64,10 @@ const Signin = () => {
         <br />
         <br />
         {!isValidUsername && (
-          <div className="messageUsername">{messageUsername}</div>
+          <div className="messageUsername">{errorsusername}</div>
         )}
         {!isValidPassword && (
-          <div className="messagePassword">{messagePassword}</div>
+          <div className="messagePassword">{errorspassword}</div>
         )}
         <button className="submit" type="submit">
           Sign in
