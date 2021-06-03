b86303788207836c90ecfd0d3d4f4dda374be9a3
shoogkabiya <shoogkabiya@hotmail.com>
2021-06-03 18:28:17 +0300

    work on css

--- src/components/Navbar.js
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./Navbar.css";
-// export default function NavBar() {
-//   return <section className="Navbar"></section>;
-// }
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 const Signup = () => {
   const history = useHistory();
