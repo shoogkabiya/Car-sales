245c8b32fe513eca057cd52ec3f2c91704336e37
shoogkabiya <shoogkabiya@hotmail.com>
2021-06-16 15:38:45 +0300

    work on css

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
+// import "./Signin.css";
 
 const Signin = () => {
   const [username, setUsername] = useState("");
