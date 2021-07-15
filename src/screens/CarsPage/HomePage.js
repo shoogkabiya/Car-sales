016bb9e44a404c26f83fd3607d66f7145eb23883
shoogkabiya <shoogkabiya@hotmail.com>
2021-07-15 22:51:39 +0300

    changes

--- src/screens/CarsPage/HomePage.js
+++ src/screens/CarsPage/HomePage.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 //icons imports
 import { AddIcon } from "../../icons/index";
 const HomePage = () => {
-  const [uploading, setUploading] = useState(false);
+  const inputRef = useRef();
+  const [File, setFile] = useState();
+  // const [uploading, setUploading] = useState(false);
   // const [images, setImages] = useState([]);
+
   const handleClick = (e) => {
-    console.log("upploading");
+    // setUploading(true);
+    // console.log(uploading);
+    const { current } = inputRef;
+    (current || { click: () => {} }).click();
   };
+
   return (
     <div className="HomePage">
       <h1>HomePage</h1>
-      <button type="file" onClick={handleClick}>
-        <AddIcon className="Add-icon" />
+      <input
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+        id="select-file"
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        ref={inputRef}
+      />
+
+      <button onClick={handleClick}>
+        <AddIcon className="Add-icon">
+          <input type="file" />
+        </AddIcon>
       </button>
+      <img src={File} />
     </div>
   );
 };
