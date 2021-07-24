1392e64be0cb796098ee549bc11a7096f40bef67
shoogkabiya <shoogkabiya@hotmail.com>
2021-07-24 23:57:52 +0300

    add some changes

--- src/components/UploadCars/UploadForm.js
+++ src/components/UploadCars/UploadForm.js
@@ -56,37 +56,36 @@ const UploadForm = () => {
       <div className="error">
         {error && <div className="errormessage">{error}</div>}
         {file && <div style={{ display: "none" }}>{file.name}</div>}
-
-        <p className="Vehicle details">Vehicle details</p>
-        <input type="text" placeholder="Version" />
-        <br />
-        <br />
-        <input type="text" placeholder="year" />
-        <br />
-        <br />
-        <input type="text" placeholder="Engine" />
-        <br />
-        <br />
-        <input type="text" placeholder="Current mileage" />
-        <br />
-        <br />
-        <input type="text" placeholder="Hand" />
-        <br />
-        <br />
-        <input type="text" placeholder="Gearbox" />
-        <br />
-        <br />
-        <input type="text" placeholder="Color" />
-        <br />
-        <br />
-        <input type="text" placeholder=" Original ownership" />
-        <br />
-        <br />
-        <button className="AddCar" type="submit">
-          Add
-        </button>
-
-        <HomePage />
+        <div className="UploadForm">
+          <p className="Vehicledetails">Vehicle details</p>
+          <input type="text" placeholder="Version" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="year" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="Engine" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="Current mileage" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="Hand" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="Gearbox" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder="Color" />
+          {/* <br />
+          <br /> */}
+          <input type="text" placeholder=" Original ownership" />
+          {/* <br />
+          <br /> */}
+          <button className="AddCar" type="submit">
+            Add
+          </button>
+        </div>
       </div>
     </form>
   );
